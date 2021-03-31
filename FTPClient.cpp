00764f3f6df9f3d3bc1482f8c6363b88deea124c
geremiapompei <geremia.pompei@studenti.unicam.it>
2021-03-31 00:36:17 -0700

    Passati parametri esplicitamente nell'init.

--- FTPClient.cpp
+++ FTPClient.cpp
@@ -31,17 +31,31 @@ string getFileName(string source, string dest)
 }
 
 /**
+ * Metodo utile a fornire alla curl i vari parametri per la connessione.
+ */
+void FTPClient::connection(CURL *curl, string extensionUrl)
+{
+    curl_easy_setopt(curl, CURLOPT_URL, (url_ + extensionUrl).c_str());
+    curl_easy_setopt(curl, CURLOPT_PORT, port_.c_str());
+    curl_easy_setopt(curl, CURLOPT_USERNAME, username_.c_str());
+    curl_easy_setopt(curl, CURLOPT_PASSWORD, password_.c_str());
+}
+
+/**
  * Implementazione del metodo init utile per inizializzare la stringa della connessione.
  */
-bool FTPClient::init(string url)
+bool FTPClient::init(string url, string port, string username, string password)
 {
     bool success = false;
     url_ = url;
+    port_ = port;
+    username_ = username;
+    password_ = password;
     CURL *curl;
     CURLcode res;
     curl = curl_easy_init();
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url_.c_str());
+        connection(curl, "");
         curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
@@ -57,7 +71,7 @@ bool FTPClient::init(string url)
 bool FTPClient::send(const char* fssource, const char* ftpdest)
 {
     bool success = false;
-    string dest = url_ + getFileName(fssource, ftpdest);
+    string dest = getFileName(fssource, ftpdest);
     struct stat file_info;
     if(stat(fssource, &file_info))
         return false;
@@ -70,9 +84,9 @@ bool FTPClient::send(const char* fssource, const char* ftpdest)
     curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
     if(curl) {
+        connection(curl, dest);
         curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
         curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-        curl_easy_setopt(curl, CURLOPT_URL, dest.c_str());
         curl_easy_setopt(curl, CURLOPT_READDATA, hd_src);
         curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)fsize);
         res = curl_easy_perform(curl);
@@ -100,8 +114,7 @@ bool FTPClient::receive(const char* ftpsource, const char* fsdest)
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
     if(curl) {
-        string fullsource = url_+ ftpsource;
-        curl_easy_setopt(curl, CURLOPT_URL, fullsource.c_str());
+        connection(curl, ftpsource);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, out);
         res = curl_easy_perform(curl);
@@ -109,8 +122,7 @@ bool FTPClient::receive(const char* ftpsource, const char* fsdest)
         if(CURLE_OK == res)
             success = true;
     } 
-    if(out)
-        fclose(out);
+    fclose(out);
     curl_global_cleanup();
     if (!success)
         remove(dest.c_str());
--- FTPClient.hpp
+++ FTPClient.hpp
@@ -9,15 +9,32 @@
 class FTPClient {
 private:
     /**
-     * Stringa di connessione ftp avente formato: ftp://username:password@ip:port
+     * Stringa di connessione ftp avente formato: ftp://domainname/
      */
     string url_;
+    /**
+     * Numero di porta del server ftp.
+     */
+    string port_;
+    /**
+     * Username utile ad identificare il profilo dell'utente connesso.
+     */
+    string username_;
+    /**
+     * Password relativa all'username.
+     */
+    string password_;
+    
+    /**
+     * Metodo utile a fornire alla curl i vari parametri per la connessione.
+     */
+    void connection(CURL *curl, string extensionUrl);
     
 public:
     /**
      * Metodo utile per inizializzare la stringa di connessione.
      */
-    bool init(string url);
+    bool init(string url, string port, string username, string password);
     
     /**
      * Metodo utile ad inviare il file locale fssource alla directory remota ftpdest.
